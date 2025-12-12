f2fa9718f76385530832b5c228e10c9462e9b01d
isratbiswas <isratbiswas28@gmail.com>
2025-12-12 19:08:56 +0600

    create order modules

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -7,41 +7,48 @@ import { User } from "../modules/user/user.model";
 import httpStatus from "http-status-codes";
 import { IsActive } from "../modules/user/user.interface";
 
-
-export const checkAuth = (...authRoles: string[]) => async(req: Request, res: Response, next: NextFunction) =>{
-    console.log(authRoles,"user-0");
-    try{
-        const accessToken = req.cookies?.accessToken ||
-      req.headers.authorization;
-        console.log(accessToken , "user-1");
-        if(!accessToken){
-            throw new ApiError(403, "No token Recieved")
-        }
-        const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET) as JwtPayload
-        console.log(verifiedToken, "user-2");
-        const isUserExist = await User.findOne({email: verifiedToken.email})
-        console.log(isUserExist, "user-4");
-        if(!isUserExist){
-           throw new ApiError(httpStatus.BAD_REQUEST, "User does not exist")
-        }
-         if (!isUserExist.isVerified) {
-            throw new ApiError(httpStatus.BAD_REQUEST, "User is not verified")
-        }
-       if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
-            throw new ApiError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
-        }
-        if (isUserExist.isDeleted) {
-            throw new ApiError(httpStatus.BAD_REQUEST, "User is deleted")
-        
+export const checkAuth =
+  (...authRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(authRoles, "user-0");
+    try {
+      const accessToken = req.cookies?.accessToken || req.headers.authorization;
+      console.log(accessToken, "user-1");
+      if (!accessToken) {
+        throw new ApiError(403, "No token Recieved");
+      }
+      const verifiedToken = verifyToken(
+        accessToken,
+        envVars.JWT_ACCESS_SECRET
+      ) as JwtPayload;
+      console.log(verifiedToken, "user-2");
+      const isUserExist = await User.findOne({ email: verifiedToken.email });
+      console.log(isUserExist, "user-4");
+      if (!isUserExist) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "User does not exist");
+      }
+      if (!isUserExist.isVerified) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "User is not verified");
+      }
+      if (
+        isUserExist.isActive === IsActive.BLOCKED ||
+        isUserExist.isActive === IsActive.INACTIVE
+      ) {
+        throw new ApiError(
+          httpStatus.BAD_REQUEST,
+          `User is ${isUserExist.isActive}`
+        );
+      }
+      if (isUserExist.isDeleted) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "User is deleted");
+      }
+      if (!authRoles.includes(verifiedToken.role)) {
+        throw new ApiError(403, "You are not permitted to view this route!!!");
+      }
+      req.user = verifiedToken;
+      next();
+    } catch (error) {
+      console.log("jwt error", error);
+      next(error);
     }
-    if(!authRoles.includes(verifiedToken.role)){
-        throw new ApiError(403, "You are not permitted to view this route!!!")
-    }
-    req.user = verifiedToken
-    next();
-}
-catch (error) {
-    console.log("jwt error", error);
-    next(error)
-}
-}
+  };
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -3,13 +3,28 @@ import { ProductControllers } from "./product.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
 
-
 const router = Router();
 
-router.get("/all-products", checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER) , ProductControllers.allProducts),
-router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER) , ProductControllers.productDetails),
-router.post("/create-product", ProductControllers.createProduct),
-router.patch("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER) , ProductControllers.updateProduct),
-router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER) , ProductControllers.deleteProduct)
-router.get("/low-stock", checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER) , ProductControllers.getLowOrOutOfStockProducts);
+(router.get(
+  "/all-products",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER),
+  ProductControllers.allProducts
+),
+  router.get(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER),
+    ProductControllers.productDetails
+  ),
+  router.post("/create-product", ProductControllers.createProduct),
+  router.patch(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER),
+    ProductControllers.updateProduct
+  ),
+  router.delete(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.MANAGER),
+    ProductControllers.deleteProduct
+  ));
+router.get("/low-stock", ProductControllers.getLowOrOutOfStockProducts);
 export const ProductRoutes = router;
